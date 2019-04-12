ee869181ea3eacb30cddb24aa7082a0c807c3373
PennaLai <525772737@qq.com>
2019-04-12 13:00:21 +0800

    增加登陆限制，添加个人主页

--- app/src/main/java/com/example/icecream/LoginActivity.java
+++ app/src/main/java/com/example/icecream/LoginActivity.java
@@ -7,17 +7,19 @@ import com.rengwuxian.materialedittext.MaterialEditText;
 import mehdi.sakout.fancybuttons.FancyButton;
 
 public class LoginActivity extends AppCompatActivity {
-    MaterialEditText username;
-    MaterialEditText password;
+    MaterialEditText usernameEdit;
+    MaterialEditText passwordEdit;
     FancyButton login;
+    FancyButton signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        username = (MaterialEditText) findViewById(R.id.username);
-        password = (MaterialEditText) findViewById(R.id.password);
+        usernameEdit = (MaterialEditText) findViewById(R.id.username);
+        passwordEdit = (MaterialEditText) findViewById(R.id.password);
         login = (FancyButton) findViewById(R.id.login);
+        signUp = (FancyButton) findViewById(R.id.signup);
     }
 
 
@@ -26,8 +28,28 @@ public class LoginActivity extends AppCompatActivity {
      * @Description: click login event
      */
     public void onLogin(View view){
-        System.out.println(username.getText());
-        System.out.println(password.getText());
-        Toast.makeText(LoginActivity.this, username.getText(),  Toast.LENGTH_LONG).show();
+        checkLoginValid();
     }
+
+    /**
+     * @author: Penna
+     * @Description: check the usernameEdit and passwordEdit valid
+     */
+    public void checkLoginValid(){
+        String username = usernameEdit.getText().toString();
+        String password = passwordEdit.getText().toString();
+        if (username.equals("") || password.equals("")){
+            Toast.makeText(LoginActivity.this, "username or password should not be empty",  Toast.LENGTH_LONG).show();
+        }else if(true){
+            // TODO check more restriction that username and password should satisfy
+        } else {
+            Toast.makeText(LoginActivity.this, "login successful",  Toast.LENGTH_LONG).show();
+            // TODO try to connect server to login
+        }
+    }
+
+    public void onSignUp(View view){
+        Toast.makeText(LoginActivity.this, "sing up successfully",  Toast.LENGTH_LONG).show();
+    }
+
 }
