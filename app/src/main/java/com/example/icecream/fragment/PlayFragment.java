4949759d34b2bbc7b5db864795a5fd4592e37b99
Aaron <Airine0119@gmail.com>
2019-05-06 11:40:12 +0800

    add play button

--- app/src/main/java/com/example/icecream/fragment/PlayFragment.java
+++ app/src/main/java/com/example/icecream/fragment/PlayFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.icecream.R;
 
@@ -17,6 +18,9 @@ public class PlayFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState ) {
-        return inflater.inflate(R.layout.fragment_play, container, false);
+        View view = inflater.inflate(R.layout.fragment_play, container, false);
+        Button btPlay = view.findViewById(R.id.bt_play);
+        btPlay.setOnClickListener(v -> System.out.println("Fuck You from play"));
+        return view;
     }
 }
