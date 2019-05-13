fde4961bd12c24f31576ad6758bfae7c9a8c329f
Aaron <Airine0119@gmail.com>
2019-05-13 11:51:03 +0800

    add article recycle list

--- app/src/main/java/com/example/icecream/fragment/ResourceFragment.java
+++ app/src/main/java/com/example/icecream/fragment/ResourceFragment.java
@@ -2,16 +2,33 @@ package com.example.icecream.fragment;
 
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.icecream.MainActivity;
 import com.example.icecream.R;
+import com.example.icecream.recycleveiw.RecycleAdapter;
+
+import java.util.Objects;
 
 
-public class ResourceFragment extends Fragment {
+public class ResourceFragment extends Fragment implements RecycleAdapter.ListItemClickListener {
+
+  private static final int NUM_LIST_ITEMS = 100;
+
+  private RecycleAdapter mAdapter;
+  private RecyclerView mArticleList;
+  private Context mainAppContext;
+
+  private Toast mToast;
 
   public static ResourceFragment newInstance() {
     return new ResourceFragment();
@@ -40,8 +57,60 @@ public class ResourceFragment extends Fragment {
       LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
     View view = inflater.inflate(R.layout.fragment_resource, container, false);
-    Button btgoToPersonalPage = view.findViewById(R.id.bt_goToPersonalDetail);
-    btgoToPersonalPage.setOnClickListener(v -> System.out.println("Fuck You from resource"));
+
+    mArticleList = view.findViewById(R.id.rv_source);
+
+    try {
+      mainAppContext = Objects.requireNonNull(getActivity()).getApplicationContext();
+      LinearLayoutManager layoutManager = new LinearLayoutManager(mainAppContext);
+      mArticleList.setLayoutManager(layoutManager);
+    } catch (java.lang.NullPointerException npe){
+      npe.printStackTrace();
+    }
+
+    mArticleList.setHasFixedSize(true);
+
+    mAdapter = new RecycleAdapter(NUM_LIST_ITEMS, this);
+    mArticleList.setAdapter(mAdapter);
+
     return view;
   }
+
+  // COMPLETED (10) Override ListItemClickListener's onListItemClick method
+  /**
+   * This is where we receive our callback from
+   * {@link RecycleAdapter.ListItemClickListener}
+   *
+   * This callback is invoked when you click on an item in the list.
+   *
+   * @param clickedItemIndex Index in the list of the item that was clicked.
+   */
+  @Override
+  public void onListItemClick(int clickedItemIndex) {
+    // COMPLETED (11) In the beginning of the method, cancel the Toast if it isn't null
+    /*
+     * Even if a Toast isn't showing, it's okay to cancel it. Doing so
+     * ensures that our new Toast will show immediately, rather than
+     * being delayed while other pending Toasts are shown.
+     *
+     * Comment out these three lines, run the app, and click on a bunch of
+     * different items if you're not sure what I'm talking about.
+     */
+    if (mToast != null) {
+      mToast.cancel();
+    }
+
+    // COMPLETED (12) Show a Toast when an item is clicked, displaying that item number that was clicked
+    /*
+     * Create a Toast and store it in our Toast field.
+     * The Toast that shows up will have a message similar to the following:
+     *
+     *                     Item #42 clicked.
+     */
+    String toastMessage = "Item #" + clickedItemIndex + " clicked.";
+    mToast = Toast.makeText(mainAppContext, toastMessage, Toast.LENGTH_LONG);
+
+    mToast.show();
+  }
+
 }
