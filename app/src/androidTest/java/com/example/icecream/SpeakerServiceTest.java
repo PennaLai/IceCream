1c30f535e3c5cdfecd43e2908806ed43a2cf0c7d
Penna <525772737@qq.com>
2019-05-13 20:01:45 +0800

    添加音乐测试，虽然没有成功

---
+++ app/src/androidTest/java/com/example/icecream/SpeakerServiceTest.java
@@ -0,0 +1,46 @@
+package com.example.icecream;
+
+import static org.junit.Assert.*;
+
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ServiceTestRule;
+import java.util.concurrent.TimeoutException;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class SpeakerServiceTest {
+
+
+  @Rule
+  public final ServiceTestRule mServiceRule = new ServiceTestRule();
+
+
+  @Test
+  public void startMusic() {
+    Intent serviceIntent =
+        new Intent(InstrumentationRegistry.getTargetContext(),
+            SpeakerService.class);
+
+    // Data can be passed to the service via the Intent.
+//    serviceIntent.putExtra(SpeakerService.SEED_KEY, 42L);
+
+    // Bind the service and grab a reference to the binder.
+    IBinder binder = null;
+    try {
+      binder = mServiceRule.bindService(serviceIntent);
+    } catch (TimeoutException e) {
+      e.printStackTrace();
+    }
+
+    // Get the reference to the service, or you can call
+    // public methods on the binder directly.
+    SpeakerService service =
+        ((SpeakerService.SpeakerBinder) binder).getService();
+
+    // Verify that the service is working correctly.
+    service.startMusic();
+//    assertThat(service.startMusic(), is(any(Integer.class)));
+  }
+}
