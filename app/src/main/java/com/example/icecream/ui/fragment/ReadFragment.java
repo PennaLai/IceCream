4e2ca0e415f5801bd9357802ba8a8551ab83ecf6
Aaron <Airine0119@gmail.com>
2019-05-21 20:47:21 +0800

    finish

--- app/src/main/java/com/example/icecream/ui/fragment/ReadFragment.java
+++ app/src/main/java/com/example/icecream/ui/fragment/ReadFragment.java
@@ -5,11 +5,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import com.example.icecream.R;
+import com.example.icecream.ui.activity.MainActivity;
 
 public class ReadFragment extends Fragment{
 
   private static final String TAG = PlayFragment.class.getName();
+
+  private ImageView iVBack;
 
   public static ReadFragment newInstance() {
     return new ReadFragment();
@@ -18,9 +22,18 @@ public class ReadFragment extends Fragment{
   @Override
   public View onCreateView(
       @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_play, container, false);
-
+    View view = inflater.inflate(R.layout.fragment_read, container, false);
+    iVBack = view.findViewById(R.id.read_iv_back);
+    iVBack.setOnClickListener(v -> backToResource());
     return view;
+  }
+
+  private void backToResource(){
+    try {
+      ((MainActivity) getActivity()).toResourceFragment();
+    } catch (NullPointerException e){
+      // do nothing
+    }
   }
 
 }
