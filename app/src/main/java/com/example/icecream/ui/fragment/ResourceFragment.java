4e762627fd75f102e397230c813aab05bab72f69
Penna <525772737@qq.com>
2019-05-22 09:30:00 +0800

    加入下拉刷新

--- app/src/main/java/com/example/icecream/ui/fragment/ResourceFragment.java
+++ app/src/main/java/com/example/icecream/ui/fragment/ResourceFragment.java
@@ -24,6 +24,9 @@ import com.example.icecream.ui.component.recycleveiw.ArticlesAdapter;
 import com.example.icecream.utils.AppViewModel;
 import com.example.icecream.utils.HttpHandler;
 
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 import java.lang.ref.WeakReference;
 import java.util.Objects;
 
@@ -122,6 +125,24 @@ public class ResourceFragment extends Fragment implements ArticlesAdapter.ListIt
 
     // observe articles from subscribed feeds
     viewModel.getPersonalArticles().observe(this, articles -> mAdapter.setArticles(articles));
+
+
+    // 下拉刷新和底部加载初始化和监听函数
+    RefreshLayout refreshLayout = (RefreshLayout)view.findViewById(R.id.refreshLayout);
+    refreshLayout.setOnRefreshListener(
+        new OnRefreshListener() {
+          @Override
+          public void onRefresh(RefreshLayout refreshlayout) {
+            refreshlayout.finishRefresh(2000 /*,false*/); // 传入false表示刷新失败
+           Loi(TAG, "onRefresh: REFERASH");
+          }
+        });
+    refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+      @Override
+      public void onLoadMore(RefreshLayout refreshlayout) {
+        refreshlayout.finishLoadMore(2000/*,false*/);//传入false表示加载失败
+      }
+    });
     return view;
   }
 
@@ -185,9 +206,13 @@ public class ResourceFragment extends Fragment implements ArticlesAdapter.ListIt
   }
 
 
+
+
   private void getRssFeeds(String phoneNumber) {
     new UpdateRssFeedsAsyncTask(this).execute(phoneNumber);
   }
+
+
 
 
   private static class UpdateRssFeedsAsyncTask extends AsyncTask<String, Void, HttpHandler.ResponseState> {
