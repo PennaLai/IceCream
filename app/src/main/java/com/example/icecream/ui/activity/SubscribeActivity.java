f4a2c402f78b96bdb5fad2d0f5e38d4acc585c09
Aaron <Airine0119@gmail.com>
2019-05-23 12:00:29 +0800

    Finish Feed Chip

--- app/src/main/java/com/example/icecream/ui/activity/SubscribeActivity.java
+++ app/src/main/java/com/example/icecream/ui/activity/SubscribeActivity.java
@@ -1,12 +1,17 @@
 package com.example.icecream.ui.activity;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.design.shape.MaterialShapeDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import com.example.icecream.R;
 import com.robertlevonyan.views.chip.Chip;
 import com.robertlevonyan.views.chip.OnSelectClickListener;
@@ -19,12 +24,13 @@ public class SubscribeActivity extends AppCompatActivity{
   static final int FENG = 2;
   static final int GEEKPARK = 3;
   static final int ZHIHU = 4;
-  private Chip chip_kr;
+//  private Chip chip_kr;
 
   private boolean [] feeds = new boolean[FEED];
   private boolean select_all = false;
 
-  private Chip[] chips = new Chip[FEED];
+  private FrameLayout[] chips = new FrameLayout[FEED];
+  private TextView[] tvs = new TextView[FEED];
 
   private Chip all;
 
@@ -33,13 +39,11 @@ public class SubscribeActivity extends AppCompatActivity{
   private Button confirm;
 
   private void initChips(){
-     boolean[] selectInfo = {false, false, false, false, false};
-    // TODO: complete function below to get the user feed selection info.
-    // selectInfo = getSelectInfo();
-    // feeds = selectInfo;
-//    for (int i = 0; i < FEED; i++) {
-//      setChipSelected(chips[i], selectInfo[i]);
-//    }
+    //     TODO: complete function below to get the user feed selection info.
+    feeds = new boolean[]{false, false, true, false, true};
+    for (int i = 0; i < FEED; i++) {
+      setChipSelected(i, feeds[i]);
+    }
 
   }
 
@@ -47,45 +51,43 @@ public class SubscribeActivity extends AppCompatActivity{
   protected void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.acitivity_subscribe);
-    chip_kr = findViewById(R.id.kr_chip);
-//    chips[KR] = findViewById(R.id.kr_chip);
+    chips[KR] = findViewById(R.id.kr_chip);
+    tvs[KR] = findViewById(R.id.kr_tv);
     chips[IFANR] = findViewById(R.id.ifanr_chip);
+    tvs[IFANR] = findViewById(R.id.ifanr_tv);
     chips[FENG] = findViewById(R.id.feng_chip);
+    tvs[FENG] = findViewById(R.id.feng_tv);
     chips[GEEKPARK] = findViewById(R.id.geekpark_chip);
+    tvs[GEEKPARK] = findViewById(R.id.geekpark_tv);
     chips[ZHIHU] = findViewById(R.id.zhihu_chip);
+    tvs[ZHIHU] = findViewById(R.id.zhihu_tv);
     all = findViewById(R.id.all_chip);
     back = findViewById(R.id.sub_iv_back);
     confirm = findViewById(R.id.sub_btn_confirm);
-//    setChipSelected(all, true);
     initChips();
 
-    /* select all(init) failed, able to debug.*/
-//    all.setOnChipClickListener(v -> {
-//      for (int i = 0; i < FEED; i++) {
-//        feeds[i] = !select_all;
-//        setChipSelected(chips[i], !select_all);
-//      }
-//      select_all = !select_all;
-//      setChipSelected(all, select_all);
-//      all.setChipText("全不选");
-//      Log.i("???", "!!!"+select_all);
-//    });
+    for (int i = 0; i < FEED; i++) {
+      int tempt = i;
+      chips[i].setOnClickListener(v -> {
+        feeds[tempt] = ! feeds[tempt];
+        setChipSelected(tempt, feeds[tempt]);
+      });
+    }
 
     all.setOnSelectClickListener((v, selected)->{
       if (selected) {
         all.setChipText("全选");
-//        for (int i = 0; i < FEED ; i ++){
-//          chip_kr.changeBackgroundColor(R.color.selected);
-//          chip_kr.setSelectable(true);
-        chip_kr.setClosable(false);
-//        }
+        for (int i = 0; i < FEED; i++) {
+          setChipSelected(i, true);
+          feeds[i] = true;
+        }
       } else {
         all.setChipText("全不选");
-//        for (int i = 0; i < FEED ; i ++){
-//        chip_kr.changeBackgroundColor(R.color.unselected);
-//        chip_kr.setSelectable(false);
-        chip_kr.setClosable(true);
-//        }
+
+        for (int i = 0; i < FEED; i++) {
+          setChipSelected(i, false);
+          feeds[i] = false;
+        }
       }
     });
 
@@ -98,22 +100,14 @@ public class SubscribeActivity extends AppCompatActivity{
     });
   }
 
-  private void selectAll(){
-
-  }
-
-  private void setChipSelected(Chip chip, boolean destinationState){
-//    chip.setSelectable(destinationState);
-//    if (destinationState) {
-//      chip.setSelectable(destinationState);
-//      chip.setStrokeColor(chips[2].getSelectedBackgroundColor());
-//      Log.i("COLOR", chips[2].getSelectedBackgroundColor()+"");
-//      chip.setTextColor(chip.getSelectedTextColor());
-//    } else {
-//      chip.setStrokeColor(chips[2].getBackgroundColor());
-//      Log.i("COLOR", chips[2].getBackgroundColor()+"");
-//      chip.setTextColor(chip.getTextColor());
-//    }
+  private void setChipSelected(int i, boolean destinationState){
+    if (destinationState) {
+      chips[i].setBackgroundDrawable(getResources().getDrawable(R.drawable.chip_selected));
+      tvs[i].setTextColor(getResources().getColor(R.color.colorChipTextClicked));
+    } else {
+      chips[i].setBackgroundDrawable(getResources().getDrawable(R.drawable.chip_unselected));
+      tvs[i].setTextColor(getResources().getColor(R.color.colorChipText));
+    }
   }
 
 }
