7633d3b920df98a99b2a5e60e4c5c492e8122184
airine <airine0119@gmail.com>
2019-05-24 19:35:40 +0800

    finish two buttons

--- app/src/main/java/com/example/icecream/ui/fragment/ReadFragment.java
+++ app/src/main/java/com/example/icecream/ui/fragment/ReadFragment.java
@@ -44,6 +44,8 @@ import com.example.icecream.utils.AppViewModel;
 import com.example.icecream.utils.HttpHandler;
 import com.example.icecream.utils.Para;
 import com.example.icecream.utils.ResourceHandler;
+import com.sackcentury.shinebuttonlib.ShineButton;
+import com.sackcentury.shinebuttonlib.ShineButton.OnCheckedChangeListener;
 import com.wang.avi.AVLoadingIndicatorView;
 import com.xw.repo.BubbleSeekBar;
 import com.xw.repo.BubbleSeekBar.OnProgressChangedListener;
@@ -51,6 +53,8 @@ import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import jp.co.recruit_lifestyle.android.widget.PlayPauseButton;
+import jp.co.recruit_lifestyle.android.widget.PlayPauseButton.OnControlStatusChangeListener;
 
 public class ReadFragment extends Fragment {
 
@@ -80,6 +84,8 @@ public class ReadFragment extends Fragment {
   /** the check whether the download is succeed. */
   private boolean downloadSucceed;
 
+  private boolean favorite = true;
+
   /** the article we are reading now. */
   private Article article;
 
@@ -92,11 +98,16 @@ public class ReadFragment extends Fragment {
   /** to update the ui progress thread. */
   Thread progressUpdateThread;
 
+  /** The play and pause button */
+  private PlayPauseButton playPauseButton;
+
   private BubbleSeekBar sbProgress;
 
   private NotificationManager musicBarManage;
 
   private RemoteViews remoteViews;
+
+  private ShineButton shineButton;
 
   /** Receive notification event. */
   BroadcastReceiver broadcastReceiver;
@@ -174,15 +185,26 @@ public class ReadFragment extends Fragment {
   public View onCreateView(
       @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_read, container, false);
-//    iVBack = view.findViewById(R.id.read_iv_back);
-//    iVBack.setOnClickListener(v -> backToResource());
+
+    shineButton = view.findViewById(R.id.read_sb_favorite);
+    shineButton.init(getActivity());
+    // TODO: initial the shineButton to checked state if already favorited
+    shineButton.setChecked(favorite);
+    shineButton.setOnCheckStateChangeListener((view14, checked) -> {
+      favorite = checked;
+    });
+
     downloadIndicator = view.findViewById(R.id.ld_download);
-//    ImageView btPlay = view.findViewById(R.id.read_iv_play);
+
     ImageView btNext = view.findViewById(R.id.read_iv_next);
-    sbProgress = view.findViewById(R.id.read_pb_progress);
-//    btPlay.setOnClickListener(v -> playBackgroundMusic());
     btNext.setOnClickListener(v -> startNextArticle());
 
+    sbProgress = view.findViewById(R.id.read_pb_progress);
+
+    playPauseButton = view.findViewById(R.id.read_play_pause_button);
+    playPauseButton.setOnControlStatusChangeListener((view13, state) -> {
+      playBackgroundMusic();
+    });
 
 //    initParagraphs();
     sbProgress.getConfigBuilder()
